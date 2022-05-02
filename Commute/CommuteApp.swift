aff126390464753669c6241fcab6bab0aa63cdf8
Aryan Kafle <aryankafle004@gmail.com>
2022-05-02 22:10:49 +0545

    Initial Commit

---
+++ Commute/CommuteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommuteApp.swift
+//  Commute
+//
+//  Created by Aryan Kafle on 02/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct CommuteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
